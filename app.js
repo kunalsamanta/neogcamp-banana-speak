6c822ef6207c67dc170c183e2355f6106d6d0e6e
Kunal Samanta <kunalsamanta5@gmail.com>
2022-11-09 21:42:09 +0530

    take input from user

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-//console.log("scrip is working properly");
+/*console.log("scrip is working properly");
 var userName= prompt("give me your name")
 var welcomeMessage="this scrip is working properly " + userName;
-alert(welcomeMessage);
+alert(welcomeMessage);*/
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input")
+console.log(txtInput);
+function clickEventHandler(){
+    console.log("clicked");
+    console.log("input " + txtInput.value);
+}
+btnTranslate.addEventListener("click", clickEventHandler);
