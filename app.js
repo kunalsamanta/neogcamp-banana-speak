a886365e6cf1895ee727e85b7247cd6c238cd6bd
Kunal Samanta <kunalsamanta5@gmail.com>
2022-11-10 01:03:49 +0530

    input output processing done

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var userName= prompt("give me your name")
 var welcomeMessage="this scrip is working properly " + userName;
 alert(welcomeMessage);*/
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput= document.querySelector("#txt-input")
-console.log(txtInput);
+var txtInput= document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#txt-output");
+//txtOutput.innerText="kunal samnata";
+
+//console.log(outputDiv);
 function clickEventHandler(){
-    console.log("clicked");
-    console.log("input " + txtInput.value);
-}
+    //console.log("clicked");
+    //console.log("input " + txtInput.value);
+    outputDiv.innerText= "translate "+txtInput.value;
+};
 btnTranslate.addEventListener("click", clickEventHandler);
